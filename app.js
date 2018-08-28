c1bceb7e347afa488c2b39de312618168ef6f561
Vibhav <vibhavsingh9415@gmail.com>
2018-08-28 20:32:15 +0000

    Added landing page

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ var express = require("express");
 var app= express();
 app.set("view engine","ejs");
 app.get("/",function(req,res){
-    res.render("landing");
+    
+    var contacts = [{firstname:"Vibhav",lastname:"Singh",contact:"9870488283"},
+    {firstname:"Vikash",lastname:"Kumar",contact:"9069769703"},
+    {firstname:"Rishabh",lastname:"Verma",contact:"9582824672"},
+    {firstname:"Lovie",lastname:"Sharma",contact:"9953051465"},
+    {firstname:"Palak",lastname:"Mehrotra",contact:"8368352195"},
+    ];
+    res.render("landing",{contacts:contacts});
 });
 
 app.listen(process.env.PORT,process.env.IP,function(){
