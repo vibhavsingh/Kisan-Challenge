afa705907c66ee33a5d0f56d0f9780e0653e80a5
Vibhav <vibhavsingh9415@gmail.com>
2018-08-28 20:05:00 +0000

    Added page routes

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app= express();
+app.set("view engine","ejs");
+app.get("/",function(req,res){
+    res.render("landing");
+});
+
+app.listen(process.env.PORT,process.env.IP,function(){
+    console.log("Server has started");
+});
